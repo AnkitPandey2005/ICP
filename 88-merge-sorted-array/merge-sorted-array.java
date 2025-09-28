ab5fd85be3ba765f644ca0dd9250225621aabeff
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-09-28 17:03:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (79.43%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void merge(int[] arr1, int m, int[] arr2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        // Merge in reverse order
+        while (i >= 0 && j >= 0) {
+            if (arr1[i] > arr2[j]) {
+                arr1[k--] = arr1[i--];
+            } else {
+                arr1[k--] = arr2[j--];
+            }
+        }
+        // If arr2 is not exhausted
+        while (j >= 0) {
+            arr1[k--] = arr2[j--];
+        }
+        // No need to handle arr1, as remaining elements are already in place
+    }
+}
