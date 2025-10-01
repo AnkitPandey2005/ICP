c2410b20d9b9dd28031dc5b876ea9e670bb9472f
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-10-01 12:37:39 +0530

    Time: 7 ms (69.84%) | Memory: 42.8 MB (58.58%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,26 +1,24 @@
 class Solution {
-  public boolean checkInclusion(String s1, String s2) {
-    int len1 = s1.length(), len2 = s2.length();
-    if (len1 > len2) return false;
-    
-    int[] count = new int[26];
-    for (int i = 0; i < len1; i++)
-        count[s1.charAt(i) - 'a']++;
-    
-    for (int i = 0; i < len2; i++) {
-           count[s2.charAt(i) - 'a']--;
-        if(i - len1 >= 0)
-          count[s2.charAt(i - len1) - 'a']++;
-        if (allZero(count)) return true;
-    }
-    
-    return false;
-}
+    public boolean checkInclusion(String p, String s) {
+        if (p.length() > s.length()) return false;
 
-  private boolean allZero(int[] count) {
-    for (int i = 0; i < 26; i++) {
-        if (count[i] != 0) return false;
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+
+        for (char c : p.toCharArray())
+            pCount[c - 'a']++;
+
+        for (int i = 0; i < s.length(); i++) {
+            sCount[s.charAt(i) - 'a']++;
+
+            if (i >= p.length()) {
+                sCount[s.charAt(i - p.length()) - 'a']--;
+            }
+
+            if (Arrays.equals(sCount, pCount)) {
+                return true;
+            }
+        }
+        return false;
     }
-    return true;
-}
 }
